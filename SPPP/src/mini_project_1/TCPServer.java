c7dfa73f21a48ce080916e876819d60f89ce4e7d
traysa <tebr@itu.dk>
2014-09-14 14:14:23 +0200

    Simplifying TCPForwarder and TCPServer

--- SPPP/src/mini_project_1/TCPServer.java
+++ SPPP/src/mini_project_1/TCPServer.java
@@ -11,7 +11,11 @@ import java.util.Date;
  * TCPServer
  *
  */
-public class TCPServer {
+public class TCPServer extends Thread{
+	
+	protected ServerSocket serverSocket = null;
+	protected Socket clientSocket = null;
+	protected boolean running = true;
 	
 	public static void main(String[] args) throws IOException {
 		// Parameter check
@@ -28,7 +32,6 @@ public class TCPServer {
 	}
 	
 	public TCPServer(int serverPort){
-		ServerSocket serverSocket;
 		try {
 			serverSocket = new ServerSocket(serverPort);
 			System.out.println(new Date()+" INFO TCPServer: Created server for port "+serverPort+"...\n");
@@ -39,150 +42,130 @@ public class TCPServer {
 
 		// Because serverSocket.accept is a blocking call, which will be only ended if a client is connecting to 
 		// the server, this call is wrapped into a thread.
-		new ServerAccept(serverSocket);
+		start();
+	}
+	
+	public void run(){
+		while(running){
+			try{
+				// Waiting for a client to connect
+				clientSocket = serverSocket.accept();
+				System.out.println (new Date()+" INFO TCPServer:\n--------------------TCP--------------------\n");
+				System.out.println(new Date()+" INFO TCPServer: Client made a connection...");
+
+				// Because the server should be able to receive several client requests, the TCP connection
+				// to clients is handled in a thread.
+				new TCPConnection(clientSocket);
+			}
+			catch(IOException e){
+				System.out.println(new Date()+" ERROR TCPServer: Something went wrong!");
+				terminate();
+			}
+		}
+		
+		if (serverSocket != null){
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(new Date()+" INFO TCPServer: Server socket closed!");
+		}
+	}
+	
+	public void terminate(){
+		running = false;
 	}
 	
 	/**
-	 * Inner class: ServerAccept
+	 * Inner Class: TCP Connection
 	 * 
-	 * Because serverSocket.accept is a blocking call, which will be only ended if a client is connecting to the server,
-	 * this call is wrapped into a thread.
+	 * Because the server should be able to receive several client requests, the TCO connection to clients 
+	 * is handled in a thread.
 	 *
 	 */
-	class ServerAccept extends Thread {
-		
-		protected ServerSocket serverSocket = null;
-		protected Socket clientSocket = null;
+	class TCPConnection extends Thread {
+		// Max packet size is 65535 bytes
+		private static final int BUFFER_SIZE = 65535;
+		// Client TCP socket used to communicate 
+		protected Socket clientSocket;
+		// Reader stream
+		protected DataInputStream socketIn;
+		// Writer stream
+		protected DataOutputStream socketOut;
+		// Controls execution of the datagram receive thread   
 		protected boolean running = true;
-		
-		public ServerAccept(ServerSocket serverSocket){
-			this.serverSocket = serverSocket;
+	    
+		public TCPConnection(Socket clientSocket) throws IOException{
+			//System.out.println(new Date()+" INFO TCPServer: Start TCP connection...");
+			this.clientSocket = clientSocket;
+			try{
+				// Connect input/output streams
+				this.socketIn = new DataInputStream(clientSocket.getInputStream());
+				this.socketOut = new DataOutputStream(clientSocket.getOutputStream());
+				//System.out.println(new Date()+" INFO TCPServer: In/Output streams connected...");
+			}
+			catch (IOException e){
+				System.out.println(new Date()+" ERROR TCPServer: Could not connect input/output streams!\n" + e.getMessage());
+				throw e;
+			}
 			start();
 		}
-		
-		public void run(){
-			while(running){
-				try{
-					// Waiting for a client to connect
-					clientSocket = serverSocket.accept();
-					System.out.println (new Date()+" INFO TCPServer:\n--------------------TCP--------------------\n");
-					System.out.println(new Date()+" INFO TCPServer: Client made a connection...");
 
-					// Because the server should be able to receive several client requests, the TCP connection
-					// to clients is handled in a thread.
-					new TCPConnection(clientSocket);
-				}
-				catch(IOException e){
-					System.out.println(new Date()+" ERROR TCPServer: Something went wrong!");
+		public void run() {
+			byte[] buffer = new byte[BUFFER_SIZE];
+			int bytesRead = 0;
+			while(running) {
+				try{
+					// Reading stream
+					System.out.println(new Date()+" INFO TCPServer: Listening for reading...");
+					bytesRead = socketIn.read(buffer);
+					System.out.println(new Date()+" INFO TCPServer: Received " +
+							bytesRead + " bytes" +
+							" from " + 
+							clientSocket.getLocalAddress() +
+							":"  + clientSocket.getLocalPort());
+					if (bytesRead==-1){
+						System.out.println(new Date()+" INFO TCPServer: Nothing more to read...");
+						terminate();
+					}
+					else {
+						// Writing stream
+						//System.out.println(new Date()+" INFO TCPServer: Listening for writing...");
+						socketOut.write(buffer, 0, bytesRead);
+						System.out.println(new Date()+" INFO TCPServer: Writing...");
+						socketOut.flush();
+					}
+					
+					yield(); //Gives other threads the chance to do something
+					
+					try {
+						//System.out.println(new Date()+" INFO TCPServer: Sleeping...");
+						Thread.sleep(1000); //Limits the number of loops per time unit for relief of the CPU
+					} catch (InterruptedException e) {
+						System.out.println(new Date()+" ERROR TCPServer: Sleeping failed!" + e.getMessage());
+					}
+				} catch (IOException e){
+					System.out.println(new Date()+" ERROR TCPServer: Read/Write error!\n" + e.getMessage());
 					terminate();
-				}
+				} 
 			}
 			
-			if (serverSocket != null){
+			if (clientSocket != null){
 				try {
-					serverSocket.close();
+					clientSocket.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				System.out.println(new Date()+" INFO TCPServer: Server socket closed!");
+				System.out.println(new Date()+" INFO TCPServer: Client socket closed!");
 			}
+			System.out.println(new Date()+" INFO TCPServer: Stop TCP Connection.");
 		}
-		
-		public void terminate(){
-			running = false;
-		}
-		
-		/**
-		 * Inner Class: TCP Connection
-		 * 
-		 * Because the server should be able to receive several client requests, the TCO connection to clients 
-		 * is handled in a thread.
-		 *
-		 */
-		class TCPConnection extends Thread {
-			// Max packet size is 65535 bytes
-			private static final int BUFFER_SIZE = 65535;
-			// Client TCP socket used to communicate 
-			protected Socket clientSocket;
-			// Reader stream
-			protected DataInputStream socketIn;
-			// Writer stream
-			protected DataOutputStream socketOut;
-			// Controls execution of the datagram receive thread   
-			protected boolean running = true;
-		    
-			public TCPConnection(Socket clientSocket) throws IOException{
-				//System.out.println(new Date()+" INFO TCPServer: Start TCP connection...");
-				this.clientSocket = clientSocket;
-				try{
-					// Connect input/output streams
-					this.socketIn = new DataInputStream(clientSocket.getInputStream());
-					this.socketOut = new DataOutputStream(clientSocket.getOutputStream());
-					//System.out.println(new Date()+" INFO TCPServer: In/Output streams connected...");
-				}
-				catch (IOException e){
-					System.out.println(new Date()+" ERROR TCPServer: Could not connect input/output streams!\n" + e.getMessage());
-					throw e;
-				}
-				start();
-			}
 
-			public void run() {
-				byte[] buffer = new byte[BUFFER_SIZE];
-				int bytesRead = 0;
-				while(running) {
-					try{
-						// Reading stream
-						System.out.println(new Date()+" INFO TCPServer: Listening for reading...");
-						bytesRead = socketIn.read(buffer);
-						System.out.println(new Date()+" INFO TCPServer: Received " +
-								bytesRead + " bytes" +
-								" from " + 
-								clientSocket.getLocalAddress() +
-								":"  + clientSocket.getLocalPort());
-						if (bytesRead==-1){
-							System.out.println(new Date()+" INFO TCPServer: Nothing more to read...");
-							terminate();
-						}
-						else {
-							// Writing stream
-							//System.out.println(new Date()+" INFO TCPServer: Listening for writing...");
-							socketOut.write(buffer, 0, bytesRead);
-							System.out.println(new Date()+" INFO TCPServer: Writing...");
-							socketOut.flush();
-						}
-						
-						yield(); //Gives other threads the chance to do something
-						
-						try {
-							//System.out.println(new Date()+" INFO TCPServer: Sleeping...");
-							Thread.sleep(1000); //Limits the number of loops per time unit for relief of the CPU
-						} catch (InterruptedException e) {
-							System.out.println(new Date()+" ERROR TCPServer: Sleeping failed!" + e.getMessage());
-						}
-					} catch (IOException e){
-						System.out.println(new Date()+" ERROR TCPServer: Read/Write error!\n" + e.getMessage());
-						terminate();
-					} 
-				}
-				
-				if (clientSocket != null){
-					try {
-						clientSocket.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-					System.out.println(new Date()+" INFO TCPServer: Client socket closed!");
-				}
-				System.out.println(new Date()+" INFO TCPServer: Stop TCP Connection.");
-			}
-
-		    public void terminate() {
-		        running = false;
-		    }
-		    
-		} // Inner Class: TCP Connection
-		
-	} //Inner class: ServerAccept
+	    public void terminate() {
+	        running = false;
+	    }
+	    
+	} // Inner Class: TCP Connection
 	
 }
